e1a26360d51005a8e2cbf62a186cf830977410d8
Austin2997 <66544274+Austin2997@users.noreply.github.com>
2020-06-17 12:29:30 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('ul');
     const navLinks = document.querySelectorAll('li');
+    
     burger.addEventListener('click',()=> {
         nav.classList.toggle('nav-active');
-    }); 
 
-    navLinks.forEach((link, index) => {
-        link.style.animation = `ulFade 0.5s ease forwards ${index / 7}s`;
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `ulFade 1s ease forwards ${index / 6 + .3}s`;
+            }
+        });
     });
+
 }
 
 navSlide();
 
-function Navhidden() {
+function navhidden() {
     var x = document.getElementById("ul");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -22,4 +28,4 @@ function Navhidden() {
     }
   }
 
-  Navhidden();
+  navhidden();
